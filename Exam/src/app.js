1f3d6f72137de849d2e7f6f0fcbac9853f35d67a
katerina-g <71485428+katerina-g@users.noreply.github.com>
2022-03-27 14:54:23 +0300

    Added JS-Application exam

---
+++ Exam/src/app.js
@@ -0,0 +1,55 @@
+import { page, render } from './lib.js'
+import { homePage } from './views/home.js';
+import { catalogPage } from './views/catalog.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { logout } from './api/data.js';
+import { getUserData } from './util.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+
+
+
+
+const root = document.getElementById('main-content');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+page('/', homePage);
+page('/catalog', catalogPage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+
+
+
+updateUserNav();
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
+
+    next();
+}
+
+async function onLogout() {
+    logout();
+    updateUserNav();
+    page.redirect('/');
+}
+
+export function updateUserNav() {
+    const userData = getUserData();
+
+    if (userData) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
